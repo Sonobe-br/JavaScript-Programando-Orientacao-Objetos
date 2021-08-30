08c9169c2e177a7985e905034ac6f544d79dc122
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2021-08-30 10:44:33 -0300

    byteBank: null & undefined, conceitos de espaço de memória, get & set, encapsulamento de classe e atributos sensiveis

--- byteBank_Clientes.js
+++ byteBank_Clientes.js
@@ -1,6 +1,5 @@
 export class Cliente {
     primeiroNome;
-    sobrenome;
     cpf;
     rg;
     endereçoResidencial;   
--- bytebank_contaCorrente.js
+++ bytebank_contaCorrente.js
@@ -1,72 +1,44 @@
-class Cliente {
-    primeiroNome;
-    sobrenome;
-    cpf;
-    rg;
-    endereçoResidencial;   
-}
-
-class ContaCorrente {
+import { Cliente } from "./byteBank_Clientes.js";
+export class ContaCorrente{
+    //dados pulicos
     agencia;
     tipoDeConta;
-    _saldo;
+    _cliente;
+
+    set cliente(newValue){
+        if(newValue instanceof Cliente){
+            this._cliente = newValue;
+        }
+    }
+
+    get cliente(){
+        return this._cliente;
+    }
+
+    //dados privado
+    saldo = 0;
 
     sacar(valor){
-        if(this._saldo >= valor) {
-            this._saldo -= valor;
-        }
-    }
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+            return valor;
+        } 
+    }     
 
     depositar(valor){
-        if(valor > 0){
-            this._saldo += valor;
+        if(valor > 0)
+        {
+        
         }
+        this.saldo += valor;
     }
+
+    //função para a realização de transferência de valores entre as contas
+    transferir(valor, conta){
+        conta.cidade = 'São Paulo';
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
+         
+    } 
 } 
 
-const cliente1 = new Cliente();
-cliente1.primeiroNome = 'Daniel';
-cliente1.sobrenome = 'Sonobe Silveira';
-cliente1.cpf = 276869748-63;
-cliente1.rg = 29024161-3;
-cliente1.endereçoResidencial = 'Rua das Ostras, 69';
-
-const contaCorrenteDaniel = new ContaCorrente();
-contaCorrenteDaniel.saldo = 0;
-contaCorrenteDaniel.agencia = 1001;
-contaCorrenteDaniel.tipoDeConta = 'Conta Corrente';
-
-
-const cliente2 = new Cliente();
-cliente2.primeiroNome = 'Alice';
-cliente2.sobrenome = 'Heineken';
-cliente2.cpf = 215696970-81;
-cliente2.rg = 24025272-4;
-cliente2.endereçoResidencial = 'Avenida Beira Mar, 69';
-
-const contaCorrenteAlice = new ContaCorrente();
-contaCorrenteAlice.saldo = 0;
-contaCorrenteAlice.agencia = 1001;
-contaCorrenteAlice.tipoDeConta = 'Conta Corrente';
-
-//console.log(cliente1, cliente2);
-console.log(contaCorrenteDaniel.saldo);
-//contaCorrenteDaniel.saldo += 100;
-contaCorrenteDaniel.depositar(10000);
-contaCorrenteDaniel.sacar(50);
-console.log(contaCorrenteDaniel)
-
-
-console.log(contaCorrenteAlice.saldo);
-//contaCorrenteAlice.saldo += 500;
-contaCorrenteAlice.depositar(2200);
-contaCorrenteAlice.sacar(100);
-console.log(contaCorrenteAlice);
-
-/* console.log(cliente1);
-console.log(cliente2); */
-
-/* PRIVATE FIELDS - Como #saldo é um campo privado, não aparece ao executarmos o console.log() da classe. 
-Só é possível acessá-lo realmente dentro da classe. Ele está sendo alterado, mas só veremos 
-isso dentro da classe. Se chamarmos o console.log() em qualquer outro lugar do código, o #saldo 
-não aparecerá. */
